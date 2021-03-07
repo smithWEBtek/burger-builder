21f23453c00bf1734802a9cd8aec34d455fd8950
brad@smithwebtek.com <brad@smithwebtek.com>
2021-03-07 13:26:48 -0500

    Thru 129

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,20 +4,28 @@ import classes from './Burger.module.css';
 
 const burger = (props) => {
   const ingredients = []
-  for(let item in props.ingredients) {
-    for(let i = 0; i < props.ingredients[item]; i++){
+  for (let item in props.ingredients) {
+    for (let i = 0; i < props.ingredients[item]; i++) {
       ingredients.push(item)
     }
   }
-  
-  const burgerIngredients = ingredients.map((ingredient, index) => { 
+
+  const transformedIngredients = ingredients.map((ingredient, index) => {
     return <BurgerIngredient key={index} type={ingredient} />
   })
+
+
+  // const transformedIngredients = Object.keys(props.ingredients)
+  //   .map((igKey) => {
+  //     return [...Array(props.ingredients[igKey])].map((_, i) => {
+  //       return <BurgerIngredient key={igKey + i} type={igKey}/>
+  //     });
+  //   })
 
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      {burgerIngredients}
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   )
